8c0ca976dff81d9f29264442fa7194a4eddf9c3b
BHasani <abdul.kadir.hasani@basajans.com>
2017-06-08 22:10:12 +0700

    button custom example

--- app/src/main/java/com/bebrayan/com/widgetprototype/MainActivity.java
+++ app/src/main/java/com/bebrayan/com/widgetprototype/MainActivity.java
@@ -2,6 +2,8 @@ package com.bebrayan.com.widgetprototype;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +11,25 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void simpleButton(View view){
+        Toast.makeText(getApplicationContext(),"Ini adalah Button Sederhana",Toast.LENGTH_SHORT).show();
+    }
+
+    public void leftIconButton(View view){
+        Toast.makeText(getApplicationContext(),"Button dengan icon dikiri",Toast.LENGTH_SHORT).show();
+    }
+
+    public void rigthIconButton(View view){
+        Toast.makeText(getApplicationContext(),"Button dengan icon dikanan",Toast.LENGTH_SHORT).show();
+    }
+
+    public void backgroundImageButton(View view){
+        Toast.makeText(getApplicationContext(),"Button dengan gambar latar",Toast.LENGTH_SHORT).show();
+    }
+
+    public void borderButton(View view){
+        Toast.makeText(getApplicationContext(),"Button dengan Border Color",Toast.LENGTH_SHORT).show();
     }
 }
