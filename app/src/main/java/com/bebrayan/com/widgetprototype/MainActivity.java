60b19ae9a855d60d83d594436f554bad329fcdc1
BHasani <abdul.kadir.hasani@basajans.com>
2017-06-10 03:42:33 +0700

    example spinner

--- app/src/main/java/com/bebrayan/com/widgetprototype/MainActivity.java
+++ app/src/main/java/com/bebrayan/com/widgetprototype/MainActivity.java
@@ -2,12 +2,43 @@ package com.bebrayan.com.widgetprototype;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Spinner;
+import android.widget.Toast;
+
 
 public class MainActivity extends Activity {
+
+    private Spinner spinner1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        spinner1 = (Spinner) findViewById(R.id.spinner);
+        spinner1.setOnItemSelectedListener(new CustomOnItemSelectedListener());
     }
+
+    public class CustomOnItemSelectedListener implements AdapterView.OnItemSelectedListener{
+
+        private String firstItem = String.valueOf(spinner1.getSelectedItem());
+
+        @Override
+        public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+            if(firstItem.equals(String.valueOf(spinner1.getSelectedItem()))){
+
+            }else {
+                Toast.makeText(parent.getContext(),"Your choose : "+parent.getItemAtPosition(position).toString(),Toast.LENGTH_LONG).show();
+            }
+        }
+
+        @Override
+        public void onNothingSelected(AdapterView<?> parent) {
+
+        }
+    }
+
+
 }
