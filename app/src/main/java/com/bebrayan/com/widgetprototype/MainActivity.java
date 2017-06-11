68d3e21a92d86f552e3f5e718034d951ccb9ef57
BHasani <abdul.kadir.hasani@basajans.com>
2017-06-11 23:19:35 +0700

    example use widget date picker

--- app/src/main/java/com/bebrayan/com/widgetprototype/MainActivity.java
+++ app/src/main/java/com/bebrayan/com/widgetprototype/MainActivity.java
@@ -2,12 +2,43 @@ package com.bebrayan.com.widgetprototype;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
+
+    private DatePicker datePicker;
+    private TextView displayDate;
+    private Button changeDate;
+    private int month;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        datePicker = (DatePicker) findViewById(R.id.datePicker);
+        displayDate = (TextView) findViewById(R.id.display_date);
+        displayDate.setText("Display Date");
+        changeDate = (Button) findViewById(R.id.change_date_button);
+
+        displayDate.setText(currentDate());
+
+        changeDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                displayDate.setText(currentDate());
+            }
+        });
+
+    }
+
+    private String currentDate(){
+        StringBuilder mcurrentDate = new StringBuilder();
+        month = datePicker.getMonth() + 1;
+        mcurrentDate.append("Date : "+month+"/"+datePicker.getDayOfMonth()+"/"+datePicker.getYear());
+        return mcurrentDate.toString();
     }
 }
