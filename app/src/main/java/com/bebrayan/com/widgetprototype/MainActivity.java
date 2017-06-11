4a9dc3d118ac30f1151bccaf50a5cf62b168b3e1
BHasani <abdul.kadir.hasani@basajans.com>
2017-06-12 03:27:51 +0700

    example use widget rating bar

--- app/src/main/java/com/bebrayan/com/widgetprototype/MainActivity.java
+++ app/src/main/java/com/bebrayan/com/widgetprototype/MainActivity.java
@@ -2,12 +2,30 @@ package com.bebrayan.com.widgetprototype;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RatingBar;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
+
+    private RatingBar ratingBar1;
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ratingBar1 = (RatingBar) findViewById(R.id.ratingbar1);
+        button = (Button) findViewById(R.id.button1);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String rating = String.valueOf(ratingBar1.getRating());
+                Toast.makeText(getApplication(),rating,Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
