7c45798e76537912fa798b8cab091da97568fe7f
BHasani <abdul.kadir.hasani@basajans.com>
2017-06-12 00:42:45 +0700

    example use widget time picker

--- app/src/main/java/com/bebrayan/com/widgetprototype/MainActivity.java
+++ app/src/main/java/com/bebrayan/com/widgetprototype/MainActivity.java
@@ -2,12 +2,39 @@ package com.bebrayan.com.widgetprototype;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.TimePicker;
 
 public class MainActivity extends Activity {
+
+    private TimePicker timePicker;
+    private TextView displayTime;
+    private Button changeTime;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        timePicker = (TimePicker) findViewById(R.id.timePicker);
+        displayTime = (TextView) findViewById(R.id.textView);
+        changeTime = (Button) findViewById(R.id.bchange_time);
+
+        timePicker.setIs24HourView(true);
+        displayTime.setText(currentTime());
+        changeTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                displayTime.setText(currentTime());
+            }
+        });
+    }
+
+    private String currentTime(){
+        String mcurrentTime = "Time : "+timePicker.getCurrentHour()+":"+timePicker.getCurrentMinute();
+        return mcurrentTime;
     }
 }
