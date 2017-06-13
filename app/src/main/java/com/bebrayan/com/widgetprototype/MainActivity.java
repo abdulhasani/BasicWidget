2534f8fb4f0326408cf58c94002f620093dfa0ad
Hasani <abdul.kadir.hasani@basajans.com>
2017-06-13 11:12:35 +0700

    example widget analogclock, run in android 2.3 fialed.

--- app/src/main/java/com/bebrayan/com/widgetprototype/MainActivity.java
+++ app/src/main/java/com/bebrayan/com/widgetprototype/MainActivity.java
@@ -2,12 +2,28 @@ package com.bebrayan.com.widgetprototype;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AnalogClock;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
+
+    private AnalogClock analogClock;
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        analogClock = (AnalogClock) findViewById(R.id.analogClock);
+        button = (Button) findViewById(R.id.button);
+        analogClock.setVisibility(View.INVISIBLE);
+
+    }
+
+    public void onButtonClicked(View view){
+        button.setText("Analog Clock");
+        analogClock.setVisibility(View.INVISIBLE);
     }
 }
