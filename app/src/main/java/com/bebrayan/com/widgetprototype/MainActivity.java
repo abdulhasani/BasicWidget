b283e35a8aa4ff5677480c65880ae0bedcf6999b
Hasani <abdul.kadir.hasani@basajans.com>
2017-06-13 14:16:55 +0700

    Popwindow

--- app/src/main/java/com/bebrayan/com/widgetprototype/MainActivity.java
+++ app/src/main/java/com/bebrayan/com/widgetprototype/MainActivity.java
@@ -1,13 +1,74 @@
 package com.bebrayan.com.widgetprototype;
 
 import android.app.Activity;
+import android.content.Context;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.PopupWindow;
+import android.widget.RelativeLayout;
 
 public class MainActivity extends Activity {
+
+    private Context context;
+    private Activity activity;
+
+    private RelativeLayout relativeLayout;
+    private Button button;
+
+    private PopupWindow popupWindow;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        context = getApplicationContext();
+        activity = MainActivity.this;
+
+        relativeLayout = relativeLayout = (RelativeLayout) findViewById(R.id.rl);
+        button = (Button) findViewById(R.id.btn);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LayoutInflater inflater = (LayoutInflater) context.getSystemService(LAYOUT_INFLATER_SERVICE);
+
+                View customView = inflater.inflate(R.layout.popup_window,null);
+                 /*
+                    public PopupWindow (View contentView, int width, int height)
+                        Create a new non focusable popup window which can display the contentView.
+                        The dimension of the window must be passed to this constructor.
+
+                        The popup does not provide any background. This should be handled by
+                        the content view.
+
+                    Parameters
+                        contentView : the popup's content
+                        width : the popup's width
+                        height : the popup's height
+                */
+                // Initialize a new instance of popup window
+                popupWindow = new PopupWindow(customView, RelativeLayout.LayoutParams.WRAP_CONTENT,
+                        RelativeLayout.LayoutParams.WRAP_CONTENT);
+
+                if(Build.VERSION.SDK_INT>=21){
+                    popupWindow.setElevation(5.0f);
+                }
+
+                ImageButton closeButton = (ImageButton) customView.findViewById(R.id.ib_close);
+                closeButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        popupWindow.dismiss();
+                    }
+                });
+                popupWindow.showAtLocation(relativeLayout, Gravity.CENTER,0,0);
+            }
+        });
     }
 }
