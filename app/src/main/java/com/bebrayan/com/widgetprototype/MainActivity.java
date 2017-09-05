ea86e315dbe23e2a83455b421572aea75e59f5d3
Hasani <abdul.kadir.hasani@basajans.com>
2017-09-05 10:47:14 +0700

    example text watcher

--- app/src/main/java/com/bebrayan/com/widgetprototype/MainActivity.java
+++ app/src/main/java/com/bebrayan/com/widgetprototype/MainActivity.java
@@ -1,13 +1,34 @@
 package com.bebrayan.com.widgetprototype;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
+
+    private EditText editText1,editText2;
+    private String text1,text2;
+    private TextView textView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().setTitle("User Login Sederhana");
+        getSupportActionBar().setSubtitle("bebrayan.com");
+
+        editText1 = (EditText) findViewById(R.id.edittext_username);
+        editText2 = (EditText) findViewById(R.id.edittext_password);
+
+        textView = (TextView) findViewById(R.id.hintpassword);
+        textView.setVisibility(View.GONE);
+
     }
+
+
 }
